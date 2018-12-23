94b5a9c99e054385f33066276136d06bbf07861d
beiwangwangchao <beiwangwangchao@sina.com>
2018-12-23 22:57:40 +0800

    no message

--- python/second/second.py
+++ python/second/second.py
@@ -1,3 +1,11 @@
-import tushare
+from sqlalchemy import create_engine
+import tushare as ts
+ts.set_token("14cf327c88cf8fc8b37a65d3ee08d17070e9092249eb6a688312f996");
+pro = ts.pro_api();
+df = ts.pro_bar(pro_api=pro, ts_code='000001.SZ', adj='qfq', start_date='20180121', end_date='20181221',ma=[5,10,30,60])
 
-print(tushare.__version__)
+print(df);
+
+#database = create_engine('mysql://root:rootroot@127.0.0.1/TESTDB?charset=utf8')
+#df.to_sql('test',database)
+df.to_csv('2.csv')
