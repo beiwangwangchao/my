f5209aa48c2b22dd179093b10c17f05ad1bf916a
beiwangwangchao <beiwangwangchao@sina.com>
2019-01-20 14:37:43 +0800

    no message

---
+++ dayfirst/src/main/java/com/zwc/dayfirst/DayfirstApplication.java
@@ -0,0 +1,14 @@
+package com.zwc.dayfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayfirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayfirstApplication.class, args);
+    }
+
+}
+
